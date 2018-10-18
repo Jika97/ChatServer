cc46ad1f86bc676441e49606c5e022376ee69978
Jikael Filomin <f.jikael@gmail.com>
2018-10-18 11:48:16 -0400

    Premiere version server.c

---
+++ server.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+int main(void){
+    
+}
